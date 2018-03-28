22ec80af8904dfba93a8b4d95ffa7317e223970b
vilmaunjan <vrunjan0014@eagle.fgcu.edu>
2018-03-27 20:42:45 -0400

    Store multiple teams and displays last known scores of stored teams. Ability to search for and browse team scores by season (year)

--- app/src/main/java/com/example/vilma/sportsscore/Tab1Activity.java
+++ app/src/main/java/com/example/vilma/sportsscore/Tab1Activity.java
@@ -4,8 +4,10 @@ package com.example.vilma.sportsscore;
  * Created by Vilma on 3/18/2018.
  */
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.media.Image;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.support.v4.app.Fragment;
@@ -15,18 +17,24 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Toast;
+
+import java.util.List;
 
 import static android.content.Context.MODE_PRIVATE;
 
 
 public class Tab1Activity extends Fragment {
 
-    private static final String LOG_TAG = NetworkUtils.class.getSimpleName();
+    private static final String LOG_TAG = Tab1Activity.class.getSimpleName();
     private ListView listTeams;
     private TeamAdapter teamAdapter;
-    String competitionId;
+    List<Team> team;
+    //String competitionId;
+    Favorites f;
+    Activity activity;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -34,22 +42,40 @@ public class Tab1Activity extends Fragment {
         super.onCreateView(inflater,container,savedInstanceState);
         View rootView = inflater.inflate(R.layout.tab1, container, false);
 
+        //For favorites
+        activity = getActivity();
+        f = new Favorites();
+
         String competitionId;
         SharedPreferences prefs = getActivity().getSharedPreferences("MyPref1",MODE_PRIVATE);
         competitionId = prefs.getString("competitionID",null);
-
-        Log.i(LOG_TAG,"SEE ID AT TAB1(SUCCESSS!!!!): COmpetition id!!!!!: "+competitionId);
 
         listTeams = (ListView) rootView.findViewById(R.id.listTeams);
         teamAdapter = new TeamAdapter(rootView.getContext(), R.layout.teams_custom_layout);
         listTeams.setAdapter(teamAdapter);
         loadTeams(competitionId);
-        listTeams.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-        @Override
-        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-            Team data =  (Team) parent.getItemAtPosition(position);
-            String teamId = data.getTeamId();
-            Toast.makeText(getActivity(), "data id: "+teamId,Toast.LENGTH_LONG).show();
+        listTeams.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+          /*      Team data = (Team) parent.getItemAtPosition(position);
+                ImageView btn = (ImageView) view.findViewById(R.id.fav);
+
+                String tag = btn.getTag().toString();
+                if (tag.equalsIgnoreCase("no")) {
+                    f.addFavorite(activity, team.get(position)); //add current to the list
+                    Toast.makeText(activity, "Added clicked team to favorites", Toast.LENGTH_LONG).show();
+                    btn.setTag("yes");
+                    btn.setImageResource(R.drawable.ic_fav);
+                } else {
+                    f.removeFavorite(activity, team.get(position));
+                    btn.setTag("no");
+                    btn.setImageResource(R.drawable.ic_unfav);
+                    Toast.makeText(activity, "Removed clicked team to favorites", Toast.LENGTH_LONG).show();
+                }
+                //String teamId = data.getTeamId();
+                //Toast.makeText(getActivity(), "data id: "+teamId,Toast.LENGTH_LONG).show();
+            */    //}
+                return true;
             }
         });
         return rootView;
